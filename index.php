dd1c6a4bb8be3d2905245980bfd27995ff17a21b
SAFA <sammoudasaffa@gmail.com>
2021-01-18 08:49:16 -0500

    dropzone php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="dropzone/dist/dropzone.js"></script>
+	<link rel="stylesheet" type="text/css" href="dropzone/dist/dropzone.css">
+
+    <title>Document</title>
+</head>
+<body>
+			<form action="upload.php" class="dropzone" id="dropzoneForm">  
+                <button type="submit" class="btn btn-info" id="submit-all">Uploader </button>
+            </form>
+    
+</body>
+</html>
